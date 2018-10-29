c1f2f1431b8c6964d275fd33c386a131f29860c2
senel34 <joshuapohan93@gmail.com>
2018-10-29 18:44:07 +0700

    setup for heroku deployment

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const weatherapp = require('./app-promise');  
 
+
+const port = process.env.PORT || 3000;
 app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -59,6 +61,6 @@ app.get('/bad', (request, response) => {
 	})
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log('Server is up on port 3000');
 });
